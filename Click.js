e5f3592472a3a993e7068748e4ac89be6756749b
Jbrown380 <77353425+Jbrown380@users.noreply.github.com>
2021-03-21 19:34:08 -0400

    Create Click.js

---
+++ Click.js
@@ -0,0 +1,5 @@
+document.getElementById("blue").addEventListener("Foo", main, h3);
+
+document.getElementById("red").addEventListener("Bar", main, h3);
+
+document.getElementById("purple").addEventListener("FooBar", main, h2);
